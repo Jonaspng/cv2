7b3354e19048ea074410fd2bceea096fa59dc6c5
Jonaspng <60882425+Jonaspng@users.noreply.github.com>
2021-08-12 23:07:26 +0800

    Add files via upload
    CV2 first version

---
+++ public/client.js
@@ -0,0 +1,4 @@
+//jshint esversion: 6
+let today = new Date();
+today = today.getFullYear();
+document.querySelector("#year").innerHTML = "Copyright © "+today+" Png Yi Wei Jonas. All rights reserved.";
